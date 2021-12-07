fad94d8f6f52eab08580e1613105900c70d9253e
Edward <edwardwinata@gmail.com>
2021-12-08 01:33:54 +0700

    renamed menu bar

--- main.py
+++ main.py
@@ -17,21 +17,22 @@ root.config(bg = "dark goldenrod")
 # Create menu bar to view tables, commands not set yet.
 Menu_Bar = Menu(root)
 More_Menu = Menu(Menu_Bar, tearoff = 0)
-More_Menu.add_command(label = 'Open Table \'Teachers\'', command = None)
+More_Menu.add_command(label = 'View Table \'Teachers\'', command = None)
 More_Menu.add_separator()
-More_Menu.add_command(label = 'Open Table \'Courses\'', command = None)
+More_Menu.add_command(label = 'View Table \'Courses\'', command = None)
 More_Menu.add_separator()
-More_Menu.add_command(label = 'Open Table \'Clients\'', command = None)
+More_Menu.add_command(label = 'View Table \'Clients\'', command = None)
 More_Menu.add_separator()
-More_Menu.add_command(label = 'Open Table \'Participants\'', command = None)
+More_Menu.add_command(label = 'View Table \'Participants\'', command = None)
 More_Menu.add_separator()
-More_Menu.add_command(label = 'Open Table \'Course_History\'', command = None)
+More_Menu.add_command(label = 'View Table \'Course_History\'', command = None)
 More_Menu.add_separator()
 More_Menu.add_command(label = 'Quit', command = root.quit)
 More_Menu.add_separator()
-Menu_Bar.add_cascade(label = 'View Tables', menu = More_Menu)
+Menu_Bar.add_cascade(label = 'Tables', menu = More_Menu)
 
-# Widgets below.
+# Label / title.
+title = Label(root)
 
 # Run app.
 if __name__ == '__main__':

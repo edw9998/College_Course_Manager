52494d45680e2c5131e52af3328225ce5c5a52ca
Edward <edwardwinata@gmail.com>
2021-12-09 04:58:35 +0700

    added treeview to display table

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 # Main page for the app.
 from tkinter import *
-import tkinter.messagebox as msb
-from tkinter.ttk import Treeview
-from PIL import Image, ImageTk
 
-from myDB import getDb
-from myDB import getDbError
+from view_and_modify.view_teachers import view_teachers_page
 
 # Main window.
 root = Tk()                                     # Create main window.
@@ -16,23 +12,61 @@ root.config(bg = "dark goldenrod")
 
 # Create menu bar to view tables, commands not set yet.
 Menu_Bar = Menu(root)
-More_Menu = Menu(Menu_Bar, tearoff = 0)
-More_Menu.add_command(label = 'View Table \'Teachers\'', command = None)
-More_Menu.add_separator()
-More_Menu.add_command(label = 'View Table \'Courses\'', command = None)
-More_Menu.add_separator()
-More_Menu.add_command(label = 'View Table \'Clients\'', command = None)
-More_Menu.add_separator()
-More_Menu.add_command(label = 'View Table \'Participants\'', command = None)
-More_Menu.add_separator()
-More_Menu.add_command(label = 'View Table \'Course_History\'', command = None)
-More_Menu.add_separator()
-More_Menu.add_command(label = 'Quit', command = root.quit)
-More_Menu.add_separator()
-Menu_Bar.add_cascade(label = 'Tables', menu = More_Menu)
+Table_Menu = Menu(Menu_Bar, tearoff = 0)
+Table_Menu.add_command(label = 'View Table \'Teachers\'', command = lambda: view_teachers_page())
+Table_Menu.add_separator()
+Table_Menu.add_command(label = 'View Table \'Courses\'', command = None)
+Table_Menu.add_separator()
+Table_Menu.add_command(label = 'View Table \'Clients\'', command = None)
+Table_Menu.add_separator()
+Table_Menu.add_command(label = 'View Table \'Participants\'', command = None)
+Table_Menu.add_separator()
+Table_Menu.add_command(label = 'View Table \'Course_History\'', command = None)
+Table_Menu.add_separator()
+Menu_Bar.add_cascade(label = 'Open Tables', menu = Table_Menu)
+
+Exit_Menu = Menu(Menu_Bar, tearoff = 0)
+Exit_Menu.add_command(label = 'Quit', command = root.quit)
+Exit_Menu.add_separator()
+Menu_Bar.add_cascade(label = 'Quit Program', menu = Exit_Menu)
 
 # Label / title.
-title = Label(root)
+title = Label(root, text = 'Course Database Management', font = ("Times New Roman", 35, "bold"), 
+                    bg = 'gray', fg = 'gray10', width = 40, justify = CENTER)
+title.place(x = -187, y = 0)
+
+'''
+Button Y - Interval = 110.0
+'''
+# Button to access table 'Teachers', command not set.
+teachers_btn = Button(root, justify = CENTER, width = 27, bg = "gold", fg = "RoyalBlue4", text = "1) Modify Table \'Teachers\'", 
+                            font = ("Times New Roman", 25, "bold"), activebackground = None, relief = GROOVE, 
+                            command = None)
+teachers_btn.place(x = 98, y = 75)
+
+# Button to access table 'Courses', command not set.
+courses_btn = Button(root, justify = CENTER, width = 27, bg = "gold", fg = "RoyalBlue4", text = "2) Modify Table \'Courses\'", 
+                           font = ("Times New Roman", 25, "bold"), activebackground = None, relief = GROOVE, 
+                           command = None)
+courses_btn.place(x = 98, y = 75 + (1 * 110.0))
+
+# Button to access table 'Clients', command not set.
+clients_btn = Button(root, justify = CENTER, width = 27, bg = "gold", fg = "RoyalBlue4", text = "3) Modify Table \'Clients\'", 
+                           font = ("Times New Roman", 25, "bold"), activebackground = None, relief = GROOVE, 
+                           command = None)
+clients_btn.place(x = 98, y = 75 + (2 * 110.0))
+
+# Button to access table 'Participants', command not set.
+participants_btn = Button(root, justify = CENTER, width = 27, bg = "gold", fg = "RoyalBlue4", text = "4) Modify Table \'Participants\'", 
+                                font = ("Times New Roman", 25, "bold"), activebackground = None, relief = GROOVE, 
+                                command = None)
+participants_btn.place(x = 98, y = 75 + (3 * 110.0))
+
+# Button to access table 'Course_History', command not set.
+course_history_btn = Button(root, justify = CENTER, width = 27, bg = "gold", fg = "RoyalBlue4", text = "5) Modify Table \'Course_History\'", 
+                                  font = ("Times New Roman", 25, "bold"), activebackground = None, relief = GROOVE, 
+                                  command = None)
+course_history_btn.place(x = 98, y = 75 + (4 * 110.0))
 
 # Run app.
 if __name__ == '__main__':
---
+++ view_and_modify/view_teachers.py
@@ -0,0 +1,72 @@
+from tkinter import *
+from tkinter import messagebox as msb
+from tkinter.ttk import Treeview
+from tkinter.ttk import Style
+from tkinter.ttk import Scrollbar
+
+from view_and_modify.myDB import getDb, getDbError
+
+def view_teachers_page():
+    win = Tk()
+    win.title("View Table : 'Teachers'")
+    win.geometry("1067x532")                            # (width x height)
+    win.resizable(height = False, width = False)
+    win.config(bg = "dark sea green")
+
+    # Retrieve all rows and keep them in cursor.
+    myDb = getDb()
+    myCursor = myDb.cursor()
+    myCursor.execute("SELECT * FROM Teachers")
+
+    tree = Treeview(win, selectmode = 'browse', height = 25)
+
+    # Omit the first empty column.
+    tree["show"] = "headings"
+
+    # Style the treeview columns and rows.
+    style = Style(win)
+    style.theme_use("clam")                                                                             # Table style.
+    style.configure("Treeview.Heading", font = ("Times New Roman", 12, "bold"), foreground = "blue")    # Heading(columns)
+    style.configure(".", font = ("Times New Roman", 12))                                                # Rows
+
+    # Define the columns.
+    tree["columns"] = ("teacher_id", "first_name", "last_name", "language_utilized", "teachingSince", "tax_id", "phone_number")
+
+    # Assign column width, min-width attributes & align column names to center.
+    tree.column("teacher_id", width = 150, minwidth = 150, anchor = CENTER)
+    tree.column("first_name", width = 150, minwidth = 150, anchor = CENTER)
+    tree.column("last_name", width = 150, minwidth = 150, anchor = CENTER)
+    tree.column("language_utilized", width = 150, minwidth = 150, anchor = CENTER)
+    tree.column("teachingSince", width = 150, minwidth = 150, anchor = CENTER)
+    tree.column("tax_id", width = 150, minwidth = 150, anchor = CENTER)
+    tree.column("phone_number", width = 150, minwidth = 150, anchor = CENTER)
+
+    # Assign column heading names.
+    tree.heading("teacher_id", text = "Teacher-ID", anchor = CENTER)
+    tree.heading("first_name", text = "First Name", anchor = CENTER)
+    tree.heading("last_name", text = "Last Name", anchor = CENTER)
+    tree.heading("language_utilized", text = "Language Utilized", anchor = CENTER)
+    tree.heading("teachingSince", text = "Teaching Since", anchor = CENTER)
+    tree.heading("tax_id", text = "Tax-ID", anchor = CENTER)
+    tree.heading("phone_number", text = "Phone Number", anchor = CENTER)
+
+    # Iterate through and insert rows from the 'teacher' table in database into treeview.
+    start = 0
+    for row in myCursor:
+        tree.insert("", start, text = "", values = (row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+        start = start + 1
+    
+    # Vertical scrollbar for treeview.
+    vsb = Scrollbar(win, orient = "vertical")
+    vsb.configure(command = tree.yview)
+
+    # Add the vertical scrollbar to the right of treeview.
+    tree.configure(yscrollcommand = vsb.set)
+    vsb.pack(fill = Y, side = RIGHT)
+
+    # Display treeview(table) at center & top positions.
+    tree.place(x = 0, y = 0)
+
+    # Display this sub-window.
+    win.mainloop()
+    return
